4107f27ae77a0fc038edec8d00d0a0f4b00a7654
Dylan Adams <113365542+max5max46@users.noreply.github.com>
2024-10-16 09:20:17 -0300

    Added All UI Screens

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    [Header("References")]
+    [SerializeField] private GameObject uiMain;
+    [SerializeField] private GameObject uiOptions;
+    [SerializeField] private GameObject uiStory;
+    [SerializeField] private GameObject uiTutorial;
+    [SerializeField] private GameObject uiUpgrades;
+    [SerializeField] private GameObject uiGameplay;
+    [SerializeField] private GameObject uiPause;
+    [SerializeField] private GameObject uiResults;
+    [SerializeField] private GameObject uiWin;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
