59f300b44298b2ca950c584790b0ad19a745542f
max5max46 <113365542+max5max46@users.noreply.github.com>
2025-01-09 22:50:13 -0400

    Added All Art and UI for Loading Screen

---
+++ Assets/Scripts/LoadingScreenManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingScreenManager : MonoBehaviour
+{
+    [Header("Properties")]
+    [SerializeField] private float slideTime;
+
+    [Header("References")]
+    [SerializeField] private Image loadingBar;
+    [SerializeField] private ProgramManager programManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
